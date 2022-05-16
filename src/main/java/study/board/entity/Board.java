3de523233dccc344edd442d681a46431df3c6b1b
chocheolhee <whcjfgml92@naver.com>
2022-05-16 16:22:06 +0900

    댓글 엔티티 추가 V1

--- src/main/java/study/board/entity/Board.java
+++ src/main/java/study/board/entity/Board.java
@@ -1,20 +1,32 @@
 package study.board.entity;
 
 import lombok.*;
+
 import javax.persistence.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Getter
 @Setter
-public class Board extends BaseEntity{
+public class Board extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_id")
     private Long id;
 
+    @Column(length = 500, nullable = false)
     private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
+
     private String writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
 }
